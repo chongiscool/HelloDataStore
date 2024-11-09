6d5854c181270060f27b7bbb8880028d050a39a3
Chong Wang <wccake@gmail.com>
2024-11-09 15:27:46 +0800

    Initialized project with SharedPreferences for key-value store

---
+++ app/src/main/java/com/wecanteen105/hellodatastore/ui/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.wecanteen105.hellodatastore.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import com.wecanteen105.hellodatastore.data.SortOrder
+import com.wecanteen105.hellodatastore.data.TasksRepository
+import com.wecanteen105.hellodatastore.data.UserPreferencesRepository
+import com.wecanteen105.hellodatastore.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val adapter = TasksAdapter()
+
+    private lateinit var viewModel: TasksViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        // findViewById(R.id.main)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        viewModel = ViewModelProvider(
+            this,
+            TasksViewModelFactory(TasksRepository, UserPreferencesRepository.getInstance(this))
+        )[TasksViewModel::class.java]
+
+        setupRecyclerView()
+        setupFilterListeners(viewModel)
+        setupSort()
+
+        viewModel.tasksUiModel.observe(this) { tasksUiModel ->
+            adapter.submitList(tasksUiModel.tasks)
+            updateSort(tasksUiModel.sortOrder)
+            binding.showCompletedSwitch.isChecked = tasksUiModel.showCompleted
+        }
+
+    }
+
+    private fun setupFilterListeners(viewModel: TasksViewModel) {
+        binding.showCompletedSwitch.setOnCheckedChangeListener { _, checked ->
+            viewModel.showCompletedTasks(checked)
+        }
+    }
+
+    private fun setupRecyclerView() {
+        // add dividers between RecyclerView's row items
+        val decoration = DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
+        binding.list.addItemDecoration(decoration)
+
+        binding.list.adapter = adapter
+    }
+
+    private fun setupSort() {
+        binding.sortDeadline.setOnCheckedChangeListener { _, checked ->
+            viewModel.enableSortByDeadline(checked)
+        }
+        binding.sortPriority.setOnCheckedChangeListener { _, checked ->
+            viewModel.enableSortByPriority(checked)
+        }
+    }
+
+    private fun updateSort(sortOrder: SortOrder) {
+        binding.sortDeadline.isChecked =
+            sortOrder == SortOrder.BY_DEADLINE || sortOrder == SortOrder.BY_DEADLINE_AND_PRIORITY
+        binding.sortPriority.isChecked =
+            sortOrder == SortOrder.BY_PRIORITY || sortOrder == SortOrder.BY_DEADLINE_AND_PRIORITY
+    }
+}
