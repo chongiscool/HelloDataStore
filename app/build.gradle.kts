c555094754d96ecf48d6e970b77a523ae04e8972
Chong Wang <wccake@gmail.com>
2024-11-11 21:14:01 +0800

    Firguring out how to configure Proto DataStore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.protobuf)
 }
 
 android {
@@ -38,6 +39,34 @@ android {
     }
 }
 
+// Setup protobuf configuration, generating lite Java and Kotlin classes
+protobuf {
+    protoc {
+        artifact = libs.protobuf.protoc.get().toString()
+//        artifact = "com.google.protobuf:protoc:3.19.4"
+    }
+    generateProtoTasks {
+        all().forEach { task ->
+            task.builtins {
+                create("java") {
+                    option("lite")
+                }
+                create("kotlin") {
+                    option("lite")
+                }
+            }
+        }
+    }
+}
+
+//androidComponents.beforeVariants {
+//    android.sourceSets.register(it.name) {
+//        val buildDir = layout.buildDirectory.get().asFile
+//        java.srcDir(buildDir.resolve("generated/source/proto/${it.name}/java"))
+//        kotlin.srcDir(buildDir.resolve("generated/source/proto/${it.name}/kotlin"))
+//    }
+//}
+
 dependencies {
 
     implementation(libs.androidx.core.ktx)
@@ -58,6 +87,8 @@ dependencies {
 
     implementation(libs.androidx.datastore.pref)
     implementation(libs.androidx.datastore)
+//    implementation(libs.protobuf.java.lite)
+    implementation(libs.protobuf.kotlin.lite)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
---
+++ app/src/main/java/com/wecanteen105/hellodatastore/data/UserPreferencesSerializer.kt
@@ -0,0 +1,5 @@
+package com.wecanteen105.hellodatastore.data
+
+import androidx.datastore.core.Serializer
+
+//object UserPreferencesSerializer:Serializer<UserPrefs>
